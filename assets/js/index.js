4cef7e71655e543864569bb017e8bfcad4749527
Brooke Mortensen <brookeamymo@gmail.com>
2018-05-22 23:09:23 -0600

    added sticky footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
+
     $("#show").click(function () {
         $(".myPortfolio").toggle();
+    });
+    
+    $("#textIcon").click(function () {
+        $("#phoneNumber").toggle();
     });
 });
 
