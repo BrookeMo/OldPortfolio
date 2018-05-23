9b8024c45420f9a736080337375d74e391eaeb4e
Brooke Mortensen <brookeamymo@gmail.com>
2018-05-22 22:26:57 -0600

    added portfolio toggle button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
-  });
+    $("#show").click(function () {
+        $(".myPortfolio").toggle();
+    });
+});
+
